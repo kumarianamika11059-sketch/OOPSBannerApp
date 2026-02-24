e1a611a447f62899232223f63bce992d459fd3b4
kumarianamika11059-sketch <kumarianamika11059@gmail.com>
2026-02-24 09:53:58 +0530

    uc2 done

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,16 @@
 public class OOPSBannerApp {
   public static void main(string[] args) {
      system.out.println("OOPS");
+     system.out.println("   ***   "+"    ***   "+" ******   "+"   *****   ");
+     system.out.println(" **   ** "+"  **   ** "+" **    ** "+" **        ");
+     system.out.println("**     ** "+"**     **"+" **     **"+"**         ");
+     system.out.println("**     ** "+"**     **"+" **    ** "+" **        ");
+     system.out.println("**     ** "+"**     **"+" ******   "+"  ***      ");
+     system.out.println("**     ** "+"**     **"+" **       "+"     **    ");
+     system.out.println("**     ** "+"**     **"+" **       "+"       **  ");
+     system.out.println(" **   **  "+" **   ** "+" **       "+"       **  ");
+     system.out.println("   ***    "+"  ***    "+" **       "+" *****     ");
   }
 }
+
+  
